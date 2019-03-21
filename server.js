e5439aec71420e2adafa37f087fc465442fa92e8
minh164 <30765881+minh164@users.noreply.github.com>
2019-03-22 00:07:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -68,8 +68,8 @@ function sendMessage(senderId, message) {
   });
 }
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "0.0.0");
+app.set('port', process.env.PORT || 5000);
+app.set('ip', process.env.IP || "0.0.0.0");
 
 server.listen(app.get('port'), app.get('ip'), function() {
   console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
