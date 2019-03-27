e0eda7d98845f352e6602a7ca9048cb7cee4496c
minh164 <30765881+minh164@users.noreply.github.com>
2019-03-27 15:31:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ var server = http.createServer(app);
 var request = require("request");
 
 app.get('/', (req, res) => {
-    console.log(1);
+    
     res.send("Home page. Server running okay.");
 });
 
 // Đây là đoạn code để tạo Webhook
 app.get('/webhook', function(req, res) {
-    console.log(2);
+    
     if (req.query['hub.verify_token'] === 'chatbot') {
         res.send(req.query['hub.challenge']);
     }
@@ -29,7 +29,7 @@ app.get('/webhook', function(req, res) {
 
 // Xử lý khi có người nhắn tin cho bot
 app.post('/webhook', function(req, res) {
-    console.log(3);
+    
     var entries = req.body.entry;
     for (var entry of entries) {
         var messaging = entry.messaging;
