adf94fade1ce2d323fa475314062dd79c521757e
minh164 <30765881+minh164@users.noreply.github.com>
2019-03-29 15:42:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const {Storage} = require('@google-cloud/storage');
 const storage = new Storage({ 
     projectId: 'my-project-1534652034762', 
-    keyFilename: 'process.env.GCS_KEYFILE'
+    credentials: 'process.env.GCS_KEYFILE'
 });
 // Makes an authenticated API request.
 storage
