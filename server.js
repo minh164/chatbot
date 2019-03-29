89d6abcddd1991ce4fb4ca8e8ce49b239ac44985
minh164 <30765881+minh164@users.noreply.github.com>
2019-03-29 17:21:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const {Storage} = require('@google-cloud/storage');
 const storage = new Storage({ 
      projectId: 'my-project-1534652034762',
      credentials: {
-          private_key: process.env.GOOGLE_PRIVATE_KEY,
+          private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
           client_email: process.env.GOOGLE_CLIENT_EMAIL
      }
-//     credentials: JSON.parse(process.env.GCS_KEYFILE) .replace(/\\n/g, '\n')
+//     credentials: JSON.parse(process.env.GCS_KEYFILE) 
 });
 // Makes an authenticated API request.
 storage
