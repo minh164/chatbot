26e326b19db95bad297f9bf4ec85b2c74bb2bf29
minh164 <30765881+minh164@users.noreply.github.com>
2019-03-29 16:18:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -108,7 +108,6 @@ app.post('/webhook', function(req, res) {
                             },
                         };
 console.log('đây là:'+sessionId);
-                        console.log(sessionClient);
                         // Send request and log result
                         const responses = await sessionClient.detectIntent(request);
                         console.log('Detected intent');
