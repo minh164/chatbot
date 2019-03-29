7eb0ee209d38901e4172538ba19c29a09dad4015
minh164 <30765881+minh164@users.noreply.github.com>
2019-03-29 16:55:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const {Storage} = require('@google-cloud/storage'); 
-const storage = new Storage({ 
-     projectId: 'my-project-1534652034762',
-     credentials: {
-          private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-          client_email: process.env.GOOGLE_CLIENT_EMAIL
-     }
-//     credentials: JSON.parse(process.env.GCS_KEYFILE)
-});
-// Makes an authenticated API request.
-storage
-  .getBuckets()
-  .then((results) => {
-    const buckets = results[0];
+// const {Storage} = require('@google-cloud/storage'); 
+// const storage = new Storage({ 
+//      projectId: 'my-project-1534652034762',
+//      credentials: {
+//           private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//           client_email: process.env.GOOGLE_CLIENT_EMAIL
+//      }
+// //     credentials: JSON.parse(process.env.GCS_KEYFILE)
+// });
+// // Makes an authenticated API request.
+// storage
+//   .getBuckets()
+//   .then((results) => {
+//     const buckets = results[0];
 
-    console.log('Buckets:');
-    buckets.forEach((bucket) => {
-      console.log(bucket.name);
-    });
-  })
-  .catch((err) => {
-    console.error('ERROR:', err);
-  });                             
+//     console.log('Buckets:');
+//     buckets.forEach((bucket) => {
+//       console.log(bucket.name);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error('ERROR:', err);
+//   });                             
 // console.log(JSON.parse(process.env.GCS_KEYFILE).project_id);
 
 const GoogleAuth = require('google-auth-library');
@@ -30,7 +30,7 @@ function authorize() {
         const authFactory = new GoogleAuth();
         const jwtClient = new authFactory.JWT(
             process.env.GOOGLE_CLIENT_EMAIL, // defined in Heroku
-            process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // defined in Heroku
+            process.env.GOOGLE_PRIVATE_KEY, // defined in Heroku
             ['https://www.googleapis.com/auth/calendar']
         );
 
