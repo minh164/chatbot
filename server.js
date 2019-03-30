1395bdfbf5de8779a5ff27f0fc94af05dd168d1e
minh164 <30765881+minh164@users.noreply.github.com>
2019-03-30 15:41:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -85,8 +85,8 @@ app.post('/webhook', function(req, res) {
                     var text = message.message.text;
                     console.log(text); // In tin nhắn người dùng
                     sendMessage(senderId, "Tui là bot đây: " + text);
-                    request.get('https://bots.dialogflow.com/facebook/f11dde88-e067-458b-82f9-a933af52a05d/webhook?hub.verify_token=bot', function(error, response, body){
-                        console.log('heheheh');
+                    request.get('https://bots.dialogflow.com/facebook/f11dde88-e067-458b-82f9-a933af52a05d/webhook?hub.verify_en=bot', function(error, response, body){
+                        
                         console.log(body);
                     });
                     /**
