66ceb5a5096d8b7eb36f9d9f64e6c3b6fc1b1e14
minh164 <30765881+minh164@users.noreply.github.com>
2019-04-02 16:46:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.post('/webhook', function(req, res) {
                          console.log(request.toString());
                          
                         // Send request and log result
-                        const responses = await sessionClient.detectIntent(text);
+                        const responses = await sessionClient.detectIntent();
                         console.log('Detected intent');
                         const result = responses[0].queryResult;
                         console.log('Query: ${result.queryText}');
