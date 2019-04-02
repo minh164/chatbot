e0266c60ab31712008ca53a4e39c6f8ab508bc76
minh164 <30765881+minh164@users.noreply.github.com>
2019-04-02 18:20:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -97,48 +97,48 @@ app.post('/webhook', function(req, res) {
                      * Send a query to the dialogflow agent, and return the query result.
                      * @param {string} projectId The project to be used
                      */
-                    async function runSample(projectId = 'my-project-1534652034762') {
-                        // A unique identifier for the given session
-                        const sessionId = uuid.v4();
-                        console.log('sesid:'+sessionId);
-                        // Create a new session
-                        const sessionClient = new dialogflow.SessionsClient({keyFilename:JSON.parse(process.env.GCS_INFO)});
-                        const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+//                     async function runSample(projectId = 'my-project-1534652034762') {
+//                         // A unique identifier for the given session
+//                         const sessionId = uuid.v4();
+//                         console.log('sesid:'+sessionId);
+//                         // Create a new session
+//                         const sessionClient = new dialogflow.SessionsClient({keyFilename:JSON.parse(process.env.GCS_INFO)});
+//                         const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-                        // The text query request.
-                        const request = {
-                            session: sessionPath,
-                            queryInput: {
-                                text: {
-                                    // The query to send to the dialogflow agent
-                                    text: text,
-                                    // The language used by the client (en-US)
-                                    languageCode: 'en-US',
-                                },
-                            },
-                        };
-                         console.log(request.toString());
+//                         // The text query request.
+//                         const request = {
+//                             session: sessionPath,
+//                             queryInput: {
+//                                 text: {
+//                                     // The query to send to the dialogflow agent
+//                                     text: text,
+//                                     // The language used by the client (en-US)
+//                                     languageCode: 'en-US',
+//                                 },
+//                             },
+//                         };
+//                          console.log(request.toString());
                          
-                        // Send request and log result
-                        const responses = await sessionClient.detectIntent(request);
-                        console.log('Detected intent');
-                         console.log(responses);
-                        const result = responses[0].queryResult;
-                        console.log('Query: ${result.queryText}');
-                        console.log('Response: ${result.fulfillmentText}');
-                        if (result.intent) {
-                            console.log('Intent: ${result.intent.displayName}');
-                        } else {
-                            console.log('No intent matched.');
-                        }
-                    }
-                    const args = process.argv.slice(2);
-                    if (args.length !== 1) {
-                      console.error('loi roiiii');
-                      process.exit(1);
-                    }
+//                         // Send request and log result
+//                         const responses = await sessionClient.detectIntent(request);
+//                         console.log('Detected intent');
+//                          console.log(responses);
+//                         const result = responses[0].queryResult;
+//                         console.log('Query: ${result.queryText}');
+//                         console.log('Response: ${result.fulfillmentText}');
+//                         if (result.intent) {
+//                             console.log('Intent: ${result.intent.displayName}');
+//                         } else {
+//                             console.log('No intent matched.');
+//                         }
+//                     }
+//                     const args = process.argv.slice(2);
+//                     if (args.length !== 1) {
+//                       console.error('loi roiiii');
+//                       process.exit(1);
+//                     }
 
-                    runSample(...args).catch(console.error);
+//                     runSample(...args).catch(console.error);
                 }
             }
         }
