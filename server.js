81f72979caa9ddb37e2d4bf1bb0bdc0d00045ecd
minh164 <30765881+minh164@users.noreply.github.com>
2019-04-02 18:10:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -132,7 +132,21 @@ app.post('/webhook', function(req, res) {
                             console.log('No intent matched.');
                         }
                     }
-                    runSample();
+                    const args = process.argv.slice(2);
+                    if (args.length !== 1) {
+                      console.error(`
+                        USAGE:
+                           node quickstart.js <projectId>
+
+                         EXAMPLE:
+                           node quickstart.js my-project-id
+
+                        You can find your project ID in your Dialogflow agent settings:  https://dialogflow.com/docs/agents#settings.
+                      `);
+                      process.exit(1);
+                    }
+
+                    runSample(...args).catch(console.error);
                 }
             }
         }
