4c99a01d478256b638060b65cdab551e20f3bc0d
minh164 <30765881+minh164@users.noreply.github.com>
2019-04-02 16:49:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -120,16 +120,16 @@ app.post('/webhook', function(req, res) {
                          console.log(request.toString());
                          
                         // Send request and log result
-                        const responses = await sessionClient.detectIntent();
-                        console.log('Detected intent');
-                        const result = responses[0].queryResult;
-                        console.log('Query: ${result.queryText}');
-                        console.log('Response: ${result.fulfillmentText}');
-                        if (result.intent) {
-                            console.log('Intent: ${result.intent.displayName}');
-                        } else {
-                            console.log('No intent matched.');
-                        }
+//                         const responses = await sessionClient.detectIntent(request);
+//                         console.log('Detected intent');
+//                         const result = responses[0].queryResult;
+//                         console.log('Query: ${result.queryText}');
+//                         console.log('Response: ${result.fulfillmentText}');
+//                         if (result.intent) {
+//                             console.log('Intent: ${result.intent.displayName}');
+//                         } else {
+//                             console.log('No intent matched.');
+//                         }
                     }
                     runSample();
 
