32592bfd3e09846f29ff92243fc787879edf2ec4
minh164 <30765881+minh164@users.noreply.github.com>
2019-04-03 14:40:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post('/webhook', function(req, res) {
                 // If user send text
                 if (message.message.text) {
                     var text = message.message.text;
-                    console.log(process.argv);
+                    
                     console.log(text); // In tin nhắn người dùng
                     sendMessage(senderId, "Tui là bot đây: " + text);
                     // gửi bằng integration
@@ -99,39 +99,38 @@ app.post('/webhook', function(req, res) {
                      * @param {string} projectId The project to be used
                      */
 //                     async function runSample(projectId = 'my-project-1534652034762') {
-//                         // A unique identifier for the given session
-//                         const sessionId = uuid.v4();
-//                         console.log('sesid:'+sessionId);
-//                         // Create a new session
-//                         const sessionClient = new dialogflow.SessionsClient({keyFilename:JSON.parse(process.env.GCS_INFO)});
-//                         const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+                        // A unique identifier for the given session
+                        const sessionId = uuid.v4();
+                        console.log('sesid:'+sessionId);
+                        // Create a new session
+                        const sessionClient = new dialogflow.SessionsClient({keyFilename:JSON.parse(process.env.GCS_INFO)});
+                        const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-//                         // The text query request.
-//                         const request = {
-//                             session: sessionPath,
-//                             queryInput: {
-//                                 text: {
-//                                     // The query to send to the dialogflow agent
-//                                     text: text,
-//                                     // The language used by the client (en-US)
-//                                     languageCode: 'en-US',
-//                                 },
-//                             },
-//                         };
+                        // The text query request.
+                        const request = {
+                            session: sessionPath,
+                            queryInput: {
+                                text: {
+                                    // The query to send to the dialogflow agent
+                                    text: text,
+                                    // The language used by the client (en-US)
+                                    languageCode: 'en-US',
+                                },
+                            },
+                        };
 
-
-//                         // Send request and log result
-//                         const responses = await sessionClient.detectIntent(request);
-//                         console.log('Detected intent');
-//                          console.log(responses);
-//                         const result = responses[0].queryResult;
-//                         console.log('Query: ${result.queryText}');
-//                         console.log('Response: ${result.fulfillmentText}');
-//                         if (result.intent) {
-//                             console.log('Intent: ${result.intent.displayName}');
-//                         } else {
-//                             console.log('No intent matched.');
-//                         }
+                        // Send request and log result
+                        const responses = await sessionClient.detectIntent(request);
+                        console.log('Detected intent');
+                         console.log(responses);
+                        const result = responses[0].queryResult;
+                        console.log('Query: ${result.queryText}');
+                        console.log('Response: ${result.fulfillmentText}');
+                        if (result.intent) {
+                            console.log('Intent: ${result.intent.displayName}');
+                        } else {
+                            console.log('No intent matched.');
+                        }
 //                     }
 //                     const args = process.argv.slice(2);
 //                     if (args.length !== 1) {
